3cc83e8787ee2b19e765d731fb3d4d221b2609aa
Rustam Gurd <rustamkurdov@mail.ru>
2017-12-03 02:49:53 +0300

    ColorFrame

---
+++ src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,30 @@
+package screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@org.springframework.stereotype.Component
+public class ColorFrame extends JFrame {
+
+    @Autowired
+    private Color color;
+
+    public ColorFrame() {
+
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace() {
+
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(700));
+        getContentPane().setBackground(color);
+        repaint();
+    }
+}
---
+++ src/main/java/screensaver/Config.java
@@ -0,0 +1,28 @@
+package screensaver;
+
+import org.springframework.context.annotation.*;
+
+import java.awt.*;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "screensaver")
+public class Config {
+    @Bean
+    public Color color() {
+        Random random = new Random();
+
+        return new Color(random.nextInt(255),
+                random.nextInt(255),
+                random.nextInt(255));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+
+        while (true) {
+            context.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(300);
+        }
+    }
+}
