93fe5a5c6543fcd1cffeeabf366cb323e5aea764
Rustam Gurd <rustamkurdov@mail.ru>
2017-12-03 01:05:52 +0300

    DeprecationHandler...

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
-//        context.getBean(Quoter.class).sayQuote();
+        context.getBean(Quoter.class).sayQuote();
     }
 }
--- src/main/java/quoters/T1000.java
+++ src/main/java/quoters/T1000.java
@@ -1,6 +1,6 @@
 package quoters;
 
-public class T1000 extends TerminatorQuoter {
+public class T1000 extends TerminatorQuoter implements Quoter{
     @Override
     public void sayQuote() {
         System.out.println("Я ЖИДКИЙ");
